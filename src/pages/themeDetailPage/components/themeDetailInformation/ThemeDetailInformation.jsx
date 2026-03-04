e794fa7719a03944fa183eb321083434b40a036a
AumKyungSub <97381242+AumKyungSub@users.noreply.github.com>
2026-03-05 01:50:25 +0900

    hundredtwentythird commit

--- src/pages/themeDetailPage/components/themeDetailInformation/ThemeDetailInformation.jsx
+++ src/pages/themeDetailPage/components/themeDetailInformation/ThemeDetailInformation.jsx
@@ -14,106 +14,137 @@ const ThemeDetailInformation = ({data, isFullMobile, themeName, lang}) => {
     }
 
     return (
-        <>
-            <section className="themeDetailCafeInfoWholeCover">
-                {!isFullMobile && <h4 className='detailTitleMin768'>{themeName} {t("themeDetail.tDCI.tDCIHInfo")}</h4>}
-                {!isFullMobile && <div className='emptyLine1px'></div>}
-                {isFullMobile && <div className='emptyLine'></div>}
-                <div className="themeDetailInformationCover">
-                    <h4 className='themeDetailInformationTitle'>{t("themeDetail.tDCI.tDCIHOperating")}</h4>
-                    <div className='themeDetailInformationTextCover'>
-                        {data?.operating?.operatingHour?.[lang]}{" "}
-                        {data?.operating?.closeDay?.[lang] && 
-                            <p className='themeDetailInformationNote'>
-                                {data?.operating?.closeDay?.[lang]}
-                            </p>}
-                        {data?.operating?.breakTime?.[lang] && 
-                            <p>
-                                {data?.operating?.breakTime?.[lang]}
-                            </p>
-                        }
-                        {data?.operating?.lastOrder?.[lang] && 
-                            <p>
-                                {data?.operating?.lastOrder?.[lang]}
-                            </p>
-                        }
-                        {data?.operating?.etc?.[lang] && 
-                            <p className='themeDetailInformationNote'>
-                                {data?.operating?.etc?.[lang]}
-                            </p>
-                        }
-                    </div>
-                </div>
-                {isFullMobile && <div className='emptyLine'></div>}
-                <div className="themeDetailInformationCover">
-                    <h4 className='themeDetailInformationTitle'>{t("themeDetail.tDCI.tDCIHMenu")}</h4>
-                    <div className='themeDetailInfoMenuCover'>
-                        <div className="themeDetailInfoMenuLeftCover">
-                            {data?.description?.menu?.[lang]?.map((item, idx) => (
-                                <span key={idx}>
-                                    <p className='themeDetailInformationText'>
-                                        {item}<br />
-                                    </p>
-                                </span>
-                            ))}
-                        </div>
-                        <div className="themeDetailInfoMenuRightCover">
-                            {data?.description?.price?.map((item, idx) => (
-                                <span key={idx}>
-                                    <p className='themeDetailInformationText'>
-                                        {item} {t("themeDetail.tDCI.tDCIHWON")}<br/>
-                                    </p>
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                    <button className='themeDetailInfoBtn' onClick={goToMenu}>
-                        <p className='subFont'>
-                            {t("themeDetail.tDCI.tDCIHAllMenu")} 
-                        </p>
-                    </button>
-                </div>
-                {isFullMobile && <div className='emptyLine'></div>}
-                <div className="themeDetailInformationCover">
-                    <h4 className='themeDetailInformationTitle'>{t("themeDetail.tDCI.tDCIHAmenities")}</h4>
-                    <div className='themeDetailInformationTextCover'>
-                        {data?.info?.parking && 
-                            <span className='themeDetailInfoOthersSpan'>
-                                <img src="/images/icon/parkingsIcon.png" alt="parking" />
-                                <p>
-                                    {t("themeDetail.tDCI.tDCIParking")}
-                                </p>
-                            </span>
-                        }
-                        {data?.info?.takeOut && 
-                            <span className='themeDetailInfoOthersSpan'>
-                                <img src="/images/icon/takeawayIcon.png" alt="takeaway" />
-                                <p>
-                                    {t("themeDetail.tDCI.tDCITake")}
-                                </p>
-                            </span>
-                        }
-                        {data?.info?.pet && 
-                            <span className='themeDetailInfoOthersSpan'>
-                                <img src="/images/icon/petIcon.png" alt="pet" />
-                                <p>
-                                    {t("themeDetail.tDCI.tDCIPet")}
-                                </p>
-                            </span>
-                        }
-                        {data?.info?.reserve && 
-                            <span className='themeDetailInfoOthersSpan'>
-                                <img src="/images/icon/bookingIcon.png" alt="reserve" />
-                                <p>
-                                    {t("themeDetail.tDCI.tDCIReserve")}
-                                </p>
-                            </span>
-                        }
-                    </div>
-                </div>
-            </section>
-            {isFullMobile && <div className='emptyLine'></div>}
-        </>
+    <>
+      <section className="themeDetailCafeInfoWholeCover">
+        {!isFullMobile && (
+          <>
+            <h4 className="detailTitleMin768">
+              {themeName} {t("themeDetail.tDCI.tDCIHInfo")}
+            </h4>
+            <div className="emptyLine1px"></div>
+          </>
+        )}
+
+        {isFullMobile && <div className="emptyLine"></div>}
+
+        {/* 운영시간 */}
+        <div className="themeDetailInformationCover">
+          <h4 className="themeDetailInformationTitle">
+            {t("themeDetail.tDCI.tDCIHOperating")}
+          </h4>
+
+          <div className="themeDetailInformationTextCover">
+            {data?.operating?.[lang]?.map((item, idx) => {
+              if (item.type === "time") {
+                return (
+                  <div key={idx} className="row">
+                    <p className='themeDetailInformationTextLabel'>{item.label}</p>
+                    <p>{item.value}</p>
+                  </div>
+                );
+              }
+
+              if (item.type === "note") {
+                return (
+                  <p key={idx} className="themeDetailInformationNote">
+                    {item.text}
+                  </p>
+                );
+              }
+
+              return null;
+            })}
+          </div>
+        </div>
+
+        {isFullMobile && <div className="emptyLine"></div>}
+
+        {/* 메뉴 */}
+<div className="themeDetailInformationCover">
+  <h4 className="themeDetailInformationTitle">
+    {t("themeDetail.tDCI.tDCIHMenu")}
+  </h4>
+
+  <div className="themeDetailInformationTextCover">
+    {data?.description?.menu?.[lang]?.map((item, idx) => {
+      const hasName = item?.name?.trim();
+      const hasPrice = item?.price?.toString().trim();
+
+      if (!hasName && !hasPrice) return null;
+
+      return (
+        <div key={idx} className="themeDetailInfoMenuCover">
+          {hasName && (
+            <p className="themeDetailInformationText">
+              {item.name}
+            </p>
+          )}
+
+          {hasName && hasPrice && (
+            <span className="dotLine" />
+          )}
+
+          {hasPrice && (
+            <p className="themeDetailInformationText">
+              {item.price}
+            </p>
+          )}
+        </div>
+      );
+    })}
+  </div>
+
+  {data?.description?.menuLink && (
+    <button className="themeDetailInfoBtn" onClick={goToMenu}>
+      <p className="subFont">
+        {t("themeDetail.tDCI.tDCIHAllMenu")}
+      </p>
+    </button>
+  )}
+</div>
+
+        {isFullMobile && <div className="emptyLine"></div>}
+
+        {/* 편의시설 */}
+        <div className="themeDetailInformationCover">
+          <h4 className="themeDetailInformationTitle">
+            {t("themeDetail.tDCI.tDCIHAmenities")}
+          </h4>
+
+          <div className="themeDetailInformationTextCover">
+            {data?.info?.parking && (
+              <span className="themeDetailInfoOthersSpan">
+                <img src="/images/icon/parkingsIcon.png" alt="parking" />
+                <p>{t("themeDetail.tDCI.tDCIParking")}</p>
+              </span>
+            )}
+
+            {data?.info?.takeOut && (
+              <span className="themeDetailInfoOthersSpan">
+                <img src="/images/icon/takeawayIcon.png" alt="takeaway" />
+                <p>{t("themeDetail.tDCI.tDCITake")}</p>
+              </span>
+            )}
+
+            {data?.info?.pet && (
+              <span className="themeDetailInfoOthersSpan">
+                <img src="/images/icon/petIcon.png" alt="pet" />
+                <p>{t("themeDetail.tDCI.tDCIPet")}</p>
+              </span>
+            )}
+
+            {data?.info?.reserve && (
+              <span className="themeDetailInfoOthersSpan">
+                <img src="/images/icon/bookingIcon.png" alt="reserve" />
+                <p>{t("themeDetail.tDCI.tDCIReserve")}</p>
+              </span>
+            )}
+          </div>
+        </div>
+      </section>
+
+      {isFullMobile && <div className="emptyLine"></div>}
+    </>
     )
 }
 
